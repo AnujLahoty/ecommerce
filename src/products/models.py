d766ece4e9cfc0e833f2ec9c872baaa69e73f2da
Anuj Lahoty <anujlahoty1997@gmail.com>
2022-05-31 00:43:28 +0530

    Created the ListView for the Product Model

--- src/products/models.py
+++ src/products/models.py
@@ -5,3 +5,6 @@ class Product(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     price = models.DecimalField(max_digits=20, decimal_places=2,default=0.00)
+    
+    def __str__(self):
+        return self.title
